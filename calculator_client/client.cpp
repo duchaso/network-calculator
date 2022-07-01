e0dd4d02654a8d465095e9824020d571fec0d3cc
Danyl Uchen <uchendanyl@gmail.com>
2022-07-01 14:28:23 +0300

    implement client-server calculator

--- calculator_client/client.cpp
+++ calculator_client/client.cpp
@@ -1,7 +1,44 @@
 #include "client.h"
 
-//Client::Client(QObject *parent)
-//    : QObject{parent}
-//{
+Client::Client(QObject *parent)
+    : QObject{parent}
+    , m_socket{new QTcpSocket(this)}
+{
+    connect(m_socket, SIGNAL(readyRead()), this, SLOT(readResult()));
+    connect(m_socket, SIGNAL(errorOccurred(QAbstractSocket::SocketError)),
+            this, SLOT(displayError(QAbstractSocket::SocketError)));
+}
 
-//}
+QString Client::result()
+{
+    return m_result;
+}
+
+void Client::setInput(QString i)
+{
+    m_input = i;
+    m_socket->abort();
+    m_socket->connectToHost("127.0.0.1", 5678);
+    m_socket->write(i.toStdString().c_str());
+}
+
+void Client::displayError(QAbstractSocket::SocketError socketError)
+{
+    switch (socketError)
+    {
+    case QAbstractSocket::RemoteHostClosedError:
+        qDebug() << "The host was not found.";
+        break;
+    case QAbstractSocket::ConnectionRefusedError:
+        qDebug() << "The connection was refused by the peer";
+        break;
+    default:
+        qDebug() << "ERROR: " << m_socket->errorString();
+    }
+}
+
+void Client::readResult()
+{
+    m_result = {m_socket->readAll()};
+    emit resultChanged();
+}
--- calculator_client/main.qml
+++ calculator_client/main.qml
@@ -1,9 +1,47 @@
 import QtQuick 2.15
 import QtQuick.Window 2.15
+import QtQuick.Controls 2.5
+import QtQuick.Layouts 1.2
+import Client 1.0
 
 Window {
     width: 640
     height: 480
     visible: true
     title: qsTr("Hello World")
+
+    Client {
+        id: client
+    }
+
+    RowLayout {
+        anchors.centerIn: parent
+
+        Rectangle {
+            height: 50
+            width: 200
+            color: "grey"
+
+            TextInput {
+                id: example
+
+                anchors.fill: parent
+            }
+        }
+        Text {
+            id: result
+
+            text: "=" + client.result
+        }
+
+        Button {
+            text: "OK"
+            onClicked: {
+                if (example.text != "")
+                {
+                    client.setInput(example.text);
+                }
+            }
+        }
+    }
 }
