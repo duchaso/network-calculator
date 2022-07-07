b5a7834e8916514ccbf38b18bb50e6c3b49066ab
Danyl Uchen <uchendanyl@gmail.com>
2022-07-07 17:05:44 +0300

    add more operation and tests

--- calculator_server/gtests/test.cpp
+++ calculator_server/gtests/test.cpp
@@ -1,15 +1,64 @@
 #include "gtest/gtest.h"
 #include "calculator.h"
 
-TEST(test1, myTest)
+TEST(Calculation, AdditionWorks)
 {
     Calculator c;
-    QString ex("2 2 -");
+    QString ex("2 2 +");
     c.calculate(ex);
-    EXPECT_EQ(c.result(), 0);
+    EXPECT_EQ(c.result(), 4);
 }
 
-TEST(test2, myTest2)
+TEST(Calculation, SubtractionWorks)
 {
-    EXPECT_EQ(1, 2);
+    Calculator c;
+    QString ex("4 2 -");
+    c.calculate(ex);
+    EXPECT_EQ(c.result(), 2);
 }
+
+TEST(Calculation, MultiplicationWorks)
+{
+    Calculator c;
+    QString ex("4 2 *");
+    c.calculate(ex);
+    EXPECT_EQ(c.result(), 8);
+}
+
+TEST(Calculation, DivisionWorks)
+{
+    Calculator c;
+    QString ex("4 2 /");
+    c.calculate(ex);
+    EXPECT_EQ(c.result(), 2);
+}
+
+TEST(Calculation, DivisionByZero)
+{
+    Calculator c;
+    QString ex("4 0 /");
+
+    try
+    {
+        c.calculate(ex);
+        FAIL() << "Expected std::runtime_error";
+    } catch(std::runtime_error& err) {
+        EXPECT_EQ(err.what(), std::string("Division by zero"));
+    } catch(...) {
+        FAIL() << "Expected std::runtime_error";
+    }
+}
+
+TEST(ClassInvariant, Correctness)
+{
+    Calculator c;
+    QString ex("hlag");
+    c.calculate(ex);
+    EXPECT_EQ(c.isCorrect(), false);
+}
+
+
+
+
+
+
